8700419eb1c72cbb3ebd5f66cc058f0324fe622f
RAMPAVITHRAN23 <rpram2003@gmail.com>
2025-03-02 18:36:51 +0530

    added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Header.css'
+import { assets } from '../../assets/assets'
 
 const Header = () => {
     return (
-        <div>
-            <Header/>
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Order your fav food here</h2>
+                <p>Choose from a wide variety of delicious meals and have them delivered to your doorstep.Enjoy exclusive discounts and offers on your favorite dishes.
+                Fast delivery and easy payment options available</p>
+            
+                <button>View menu</button>
+            </div>
         </div>
     )
 }
