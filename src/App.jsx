e30fcfaa93bea8c28689c825462df6f63681f508
RAMPAVITHRAN23 <rpram2003@gmail.com>
2025-03-04 21:44:21 +0530

    added with error

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,10 @@ const App = () => {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/placeorder' element={<PlaceOrder />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/placeorder" element={<PlaceOrder />} />
       </Routes>
-
     </div>
   );
 };
--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -1,14 +1,37 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
     
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev)=> ({...prev, [itemId]: 1}));
+        }
+        else {
+            setCartItems((prev)=> ({...prev, [itemId]: prev[itemId] + 1}));
+        }
+    }
+
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]: prev[itemId] - 1}));
+    }
+    useEffect(() => {
+        console.log(cartItems);
+    }
+    ,[cartItems]);
+
 
 
     const contextvalue = {
         food_list,
+        cartItems,
+        setCartItems,
+        removeFromcart,
+        addToCart,
 
 
     }
@@ -18,5 +41,6 @@ const StoreContextProvider = (props) => {
         </StoreContext.Provider>
     )
 }
+
 
 export default StoreContextProvider;
